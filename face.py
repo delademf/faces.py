9894d0a82473760274fa22502fd2149980c03258
delademf <106419974+delademf@users.noreply.github.com>
2024-08-10 19:49:36 +0000

    Update face.py

--- face.py
+++ face.py
@@ -1,6 +1,8 @@
+#import dependencies
 import cv2 as cv
+#read image
 img =cv.imread('imgees/h.jpg')
-
+#redefine size of image read
 def rescaleframe(frame, scale=0.75):
     width = int(frame.shape[1]*0.2)
     height = int(frame.shape[0]*0.2)
@@ -8,18 +10,25 @@ def rescaleframe(frame, scale=0.75):
     return cv.resize(frame,dimensions,interpolation=cv.INTER_AREA)
 resized_image =rescaleframe(img)
 
+#change image to grayscale
 gray = cv.cvtColor(resized_image, cv.COLOR_BGR2GRAY)
 cv.imshow('gray',gray)
 
-#link file with data on images 
+#link faces.py with data on images   NB: Download file from github
+#https://github.com/opencv/opencv/blob/4.x/data/haarcascades/haarcascade_frontalface_default.xml
+#download and save it in the same folder as his file
 haar_cascade = cv.CascadeClassifier('haar_face.xml')
 
+#place rectangle on face to show detection
 faces_rect =haar_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=3)
 
+#print number of faces detected
 print(f'number of faces = {len(faces_rect)}')
 
+#place rectangle on face to show detection
 for (x,y,w,h) in faces_rect:
     cv.rectangle(resized_image,(x,y),(x+w,y+h),(0,255,0),thickness=3)
+#show image
 cv.imshow('detected image',resized_image)
 
 
